1395a7f47999a10028ee36f2644ef949f283f0d2
renewjoy <oyea9le@qq.com>
2015-05-25 09:15:57 +0800

    update for scala

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -37,98 +37,98 @@ limit coredumpsize 0
 # Emacs风格 键绑定
 bindkey -e
 
-#自动补全功能 {{{
-setopt AUTO_LIST
-setopt AUTO_MENU
-#开启此选项，补全时会直接选中菜单项
-#setopt MENU_COMPLETE
+# #自动补全功能 {{{
+# setopt AUTO_LIST
+# setopt AUTO_MENU
+# #开启此选项，补全时会直接选中菜单项
+# #setopt MENU_COMPLETE
 
-autoload -U compinit
-compinit
+# autoload -U compinit
+# compinit
 
-#自动补全缓存
-#zstyle ':completion::complete:*' use-cache on
-#zstyle ':completion::complete:*' cache-path .zcache
-#zstyle ':completion:*:cd:*' ignore-parents parent pwd
+# #自动补全缓存
+# #zstyle ':completion::complete:*' use-cache on
+# #zstyle ':completion::complete:*' cache-path .zcache
+# #zstyle ':completion:*:cd:*' ignore-parents parent pwd
 
-#自动补全选项
-zstyle ':completion:*:match:*' original only
-zstyle ':completion::prefix-1:*' completer _complete
-zstyle ':completion:predict:*' completer _complete
-zstyle ':completion:incremental:*' completer _complete _correct
-zstyle ':completion:*' completer _complete _prefix _correct _prefix _match _approximate
+# #自动补全选项
+# zstyle ':completion:*:match:*' original only
+# zstyle ':completion::prefix-1:*' completer _complete
+# zstyle ':completion:predict:*' completer _complete
+# zstyle ':completion:incremental:*' completer _complete _correct
+# zstyle ':completion:*' completer _complete _prefix _correct _prefix _match _approximate
 
-#路径补全
-zstyle ':completion:*' expand 'yes'
-zstyle ':completion:*' squeeze-shlashes 'yes'
-zstyle ':completion::complete:*' '\\'
+# #路径补全
+# zstyle ':completion:*' expand 'yes'
+# zstyle ':completion:*' squeeze-shlashes 'yes'
+# zstyle ':completion::complete:*' '\\'
 
-zstyle ':completion:*' menu select
-zstyle ':completion:*:*:default' force-list always
+# zstyle ':completion:*' menu select
+# zstyle ':completion:*:*:default' force-list always
 
-#彩色补全菜单 
-export ZLSCOLORS="${LS_COLORS}"
-zmodload zsh/complist
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+# #彩色补全菜单 
+# export ZLSCOLORS="${LS_COLORS}"
+# zmodload zsh/complist
+# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 
-#修正大小写
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
-#错误校正      
-zstyle ':completion:*' completer _complete _match _approximate
-zstyle ':completion:*:match:*' original only
-zstyle ':completion:*:approximate:*' max-errors 1 numeric
+# #修正大小写
+# zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
+# #错误校正      
+# zstyle ':completion:*' completer _complete _match _approximate
+# zstyle ':completion:*:match:*' original only
+# zstyle ':completion:*:approximate:*' max-errors 1 numeric
 
-#kill 命令补全      
-compdef pkill=kill
-compdef pkill=killall
-zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:*:*:*:processes' force-list always
-zstyle ':completion:*:processes' command 'ps -au$USER'
+# #kill 命令补全      
+# compdef pkill=kill
+# compdef pkill=killall
+# zstyle ':completion:*:*:kill:*' menu yes select
+# zstyle ':completion:*:*:*:*:processes' force-list always
+# zstyle ':completion:*:processes' command 'ps -au$USER'
 
-#补全类型提示分组 
-zstyle ':completion:*:matches' group 'yes'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d --\e[0m'
-zstyle ':completion:*:messages' format $'\e[01;35m -- %d --\e[0m'
-zstyle ':completion:*:warnings' format $'\e[01;31m -- No Matches Found --\e[0m'
-#}}}
+# #补全类型提示分组 
+# zstyle ':completion:*:matches' group 'yes'
+# zstyle ':completion:*' group-name ''
+# zstyle ':completion:*:options' description 'yes'
+# zstyle ':completion:*:options' auto-description '%d'
+# zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d --\e[0m'
+# zstyle ':completion:*:messages' format $'\e[01;35m -- %d --\e[0m'
+# zstyle ':completion:*:warnings' format $'\e[01;31m -- No Matches Found --\e[0m'
+# #}}}
 
-##行编辑高亮模式 {{{
-# Ctrl+@ 设置标记，标记和光标点之间为 region
-zle_highlight=(region:bg=magenta #选中区域 
-              special:bold      #特殊字符
-              isearch:underline)#搜索时使用的关键字
-#}}}
+# ##行编辑高亮模式 {{{
+# # Ctrl+@ 设置标记，标记和光标点之间为 region
+# zle_highlight=(region:bg=magenta #选中区域 
+#               special:bold      #特殊字符
+#               isearch:underline)#搜索时使用的关键字
+# #}}}
 
-##空行(光标在行首)补全 "cd " {{{
-user-complete(){
-   case $BUFFER in
-       "" )                       # 空行填入 "cd "
-           BUFFER="cd "
-           zle end-of-line
-           zle expand-or-complete
-           ;;
-       "cd --" )                  # "cd --" 替换为 "cd +"
-           BUFFER="cd +"
-           zle end-of-line
-           zle expand-or-complete
-           ;;
-       "cd +-" )                  # "cd +-" 替换为 "cd -"
-           BUFFER="cd -"
-           zle end-of-line
-           zle expand-or-complete
-           ;;
-       * )
-           zle expand-or-complete
-           ;;
-   esac
-}
-zle -N user-complete
-bindkey "\t" user-complete
-#}}}
+# ##空行(光标在行首)补全 "cd " {{{
+# user-complete(){
+#    case $BUFFER in
+#        "" )                       # 空行填入 "cd "
+#            BUFFER="cd "
+#            zle end-of-line
+#            zle expand-or-complete
+#            ;;
+#        "cd --" )                  # "cd --" 替换为 "cd +"
+#            BUFFER="cd +"
+#            zle end-of-line
+#            zle expand-or-complete
+#            ;;
+#        "cd +-" )                  # "cd +-" 替换为 "cd -"
+#            BUFFER="cd -"
+#            zle end-of-line
+#            zle expand-or-complete
+#            ;;
+#        * )
+#            zle expand-or-complete
+#            ;;
+#    esac
+# }
+# zle -N user-complete
+# bindkey "\t" user-complete
+# #}}}
 
 # 使用emacsclient 的时候，如果emacs --daemon没有启动，下面这行自动启动
 export ALTERNATE_EDITOR=""
